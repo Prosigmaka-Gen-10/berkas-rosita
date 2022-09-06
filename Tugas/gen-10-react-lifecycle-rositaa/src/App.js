7dad5ec91a16698412af4281607efec7c8d7a7a9
Rosita120505 <107414498+Rosita120505@users.noreply.github.com>
2022-09-06 10:20:34 +0700

    tugas lifecycle

---
+++ Tugas/gen-10-react-lifecycle-rositaa/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import LifeCycle from "./LifeCycle";
+import LifeCycleFuntion from "./LifeCycleFunction";
+
+function App() {
+  return <LifeCycleFuntion />;
+}
+
+export default App;
